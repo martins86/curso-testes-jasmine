a4cc2b9c1d1679d5b7c9335036b8d3f24f1bb656
Thiago Martins de Freitas <martins86@gmail.com>
2023-06-04 20:31:35 -0300

    truncate strings

--- jasmine-angular/src/app/pipes/truncate-string/truncate-string.pipe.spec.ts
+++ jasmine-angular/src/app/pipes/truncate-string/truncate-string.pipe.spec.ts
@@ -5,4 +5,31 @@ describe('TruncateStringPipe', () => {
     const pipe = new TruncateStringPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should be returns the string reduced from 55 to 30 characters using "..." in the center', () => {
+    // Arrange
+    const stringTextWith55: string =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+    const formattedTextWith30: string = 'Lorem ipsum dol...piscing elit';
+    const pipe = new TruncateStringPipe();
+
+    // Act
+    const result = pipe.transform(stringTextWith55, 30, '...');
+
+    // Assert
+    expect(result).toEqual(formattedTextWith30);
+  });
+
+  it('should be does not return the reduced string from 55 to 56, it returns the original text', () => {
+    // Arrange
+    const stringTextWith55: string =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+    const pipe = new TruncateStringPipe();
+
+    // Act
+    const result = pipe.transform(stringTextWith55, 56, '...');
+
+    // Assert
+    expect(result).toEqual(stringTextWith55);
+  });
 });
--- jasmine-angular/src/app/pipes/truncate-string/truncate-string.pipe.ts
+++ jasmine-angular/src/app/pipes/truncate-string/truncate-string.pipe.ts
@@ -1,12 +1,30 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'truncateString'
+  name: 'truncateString',
 })
+/**
+ * Objective: reduce texts with more than 30 characters.
+ *
+ * Truncate string 15 + ... + 12 = 30 Chars.
+ * stringText: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+ *
+ * @returns `Lorem ipsum dol...piscing elit`
+ *
+ * @example
+ *
+ * `<p>{{ stringText | truncateString: 30 : '...' }}</p>`
+ */
 export class TruncateStringPipe implements PipeTransform {
+  transform(stringText: string, maxLength: number, replaceValue: string): any {
+    if (stringText.length > maxLength) {
+      let lastChars = stringText.length - 15 + replaceValue.length;
+      let stringA = stringText.split('').splice(0, 15);
+      let stringB = stringText.split('').splice(lastChars, stringText.length);
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+      return stringA.join('') + replaceValue + stringB.join('');
+    } else {
+      return stringText;
+    }
   }
-
 }
