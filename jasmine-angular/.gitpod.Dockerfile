2e3ef8c3a4e62f4e1ff968a9ba9f46b74e58cf47
Thiago Martins de Freitas <46196327+martins86@users.noreply.github.com>
2023-07-29 19:59:54 -0300

    Delete jasmine-angular directory

--- jasmine-angular/.gitpod.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM gitpod/workspace-full-vnc
-
-RUN sudo apt-get update \
- && sudo apt-get install -y \
-    chromium-browser \
- && sudo rm -rf /var/lib/apt/lists/*
-
-ENV CHROME_BIN=/usr/bin/chromium
--- jasmine-angular/src/app/shared/components/header-page/header-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header-page',
-  templateUrl: './header-page.component.html',
-  styleUrls: ['./header-page.component.scss'],
-  standalone: true,
-})
-export class HeaderPageComponent {}
